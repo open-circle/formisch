b2773f12a42397fee2b39a015eef0ec2492e7041
Fabian Hiller <hillerfabian11@gmail.com>
2025-12-04 11:34:04 -0500

    Improve cleanup of signal subscribers

--- frameworks/react/src/hooks/useSignals/useSignals.ts
+++ frameworks/react/src/hooks/useSignals/useSignals.ts
@@ -1,27 +1,50 @@
 import { type Listener, setListener } from '@formisch/core/vanilla';
-import { useLayoutEffect, useMemo, useState } from 'react';
+import {
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useMemo,
+  useReducer,
+  useRef,
+} from 'react';
 
 /**
  * Hook to enable reactive signals within a React component.
  */
 export function useSignals(): void {
-  // Create a state to trigger re-renders
-  const [, setTick] = useState({});
+  // Create a force update function to trigger re-renders
+  const [, forceUpdate] = useReducer((x) => x + 1, 0);
 
-  // Create listener for current component
-  const listener = useMemo<Listener>(
-    () => [() => setTick({}), new Set<Set<Listener>>()],
-    []
-  );
+  // Create listener tuple for current component
+  const listener = useMemo<Listener>(() => [forceUpdate, new Set()], []);
 
-  // Remove listener from subscribers
-  for (const subscriber of listener[1]) {
-    subscriber.delete(listener);
-  }
+  // Create cleanup function to remove listener from subscribers
+  const cleanSubscribers = useCallback(() => {
+    for (const subscriber of listener[1]) {
+      subscriber.delete(listener);
+    }
+  }, [listener]);
+
+  // Clean previously registered subscribers
+  cleanSubscribers();
 
   // Set listener for tracking signals
   setListener(listener);
 
   // Clear listener directly after render
   useLayoutEffect(() => setListener(undefined));
+
+  // Cleanup registered subscribers on unmount
+  const timeout = useRef<ReturnType<typeof setTimeout>>(null);
+  useEffect(() => {
+    // Hint: This timeout hack is necessary because React `<StrictMode>`
+    // re-runs effects twice in dev mode.
+    if (timeout.current) {
+      clearTimeout(timeout.current);
+      timeout.current = null;
+    }
+    return () => {
+      timeout.current = setTimeout(cleanSubscribers);
+    };
+  }, [cleanSubscribers]);
 }
