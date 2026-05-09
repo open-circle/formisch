d17fdf39ac3bf0b3f02c707c8be0599f1189393d
Fabian Hiller <hillerfabian11@gmail.com>
2026-05-09 15:07:55 -0400

    Remove isolate option from Vitest configuration for React package

--- frameworks/react/vitest.config.ts
+++ frameworks/react/vitest.config.ts
@@ -5,7 +5,6 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom',
-    isolate: false,
     setupFiles: ['./src/vitest/setup.ts'],
     coverage: {
       include: ['src'],
