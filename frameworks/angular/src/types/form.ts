a587099d3bb24cea5368afb082cb5e0ee07adb24
Sonu Kapoor <sonukapoor@gmail.com>
2026-05-24 17:59:16 -0400

    Add Angular FormStore, FieldStore, and FieldArrayStore types

---
+++ frameworks/angular/src/types/form.ts
@@ -0,0 +1,40 @@
+import type { Signal } from '@angular/core';
+import type { BaseFormStore, Schema } from '@formisch/core/angular';
+
+/**
+ * Form store interface.
+ */
+export interface FormStore<TSchema extends Schema = Schema>
+  extends BaseFormStore<TSchema> {
+  /**
+   * Whether the form is currently submitting.
+   */
+  readonly isSubmitting: Signal<boolean>;
+  /**
+   * Whether the form has been submitted.
+   */
+  readonly isSubmitted: Signal<boolean>;
+  /**
+   * Whether the form is currently validating.
+   */
+  readonly isValidating: Signal<boolean>;
+  /**
+   * Whether any field in the form has been touched.
+   */
+  readonly isTouched: Signal<boolean>;
+  /**
+   * Whether any field in the form differs from its initial value.
+   */
+  readonly isDirty: Signal<boolean>;
+  /**
+   * Whether the form is valid according to the schema.
+   */
+  readonly isValid: Signal<boolean>;
+  /**
+   * The current error messages of the form.
+   *
+   * Hint: This property only contains validation errors at the root level
+   * of the form. To get all errors from all fields, use `getAllErrors`.
+   */
+  readonly errors: Signal<[string, ...string[]] | null>;
+}
--- frameworks/angular/src/types/index.ts
+++ frameworks/angular/src/types/index.ts
@@ -1,1 +1,2 @@
-export {};
+export * from './field.ts';
+export * from './form.ts';
