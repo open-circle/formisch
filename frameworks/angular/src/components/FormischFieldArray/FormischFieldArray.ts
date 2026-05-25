ace8f861f58700f5b736389c6e26f5d970a82f72
Sonu Kapoor <sonukapoor@gmail.com>
2026-05-24 23:04:15 -0400

    Add FormischFieldArray standalone component

---
+++ frameworks/angular/src/components/FormischFieldArray/FormischFieldArray.ts
@@ -0,0 +1,88 @@
+import {
+  Component,
+  computed,
+  ContentChild,
+  Input,
+  input,
+  type InputSignal,
+  TemplateRef,
+} from '@angular/core';
+import { NgIf, NgTemplateOutlet } from '@angular/common';
+import {
+  getFieldBool,
+  getFieldStore,
+  INTERNAL,
+  type InternalArrayStore,
+  type RequiredPath,
+  type Schema,
+  type ValidArrayPath,
+} from '@formisch/core/angular';
+import type * as v from 'valibot';
+import type { FieldArrayStore, FormStore } from '../../types/index.ts';
+
+/**
+ * Headless field array component that provides reactive field array state via an Angular template.
+ * Uses ContentChild TemplateRef pattern to pass the FieldArrayStore as $implicit context.
+ *
+ * @example
+ * ```html
+ * <formisch-field-array [of]="form" [path]="['todos']">
+ *   <ng-template let-fieldArray>
+ *     @for (item of fieldArray.items(); track item) {
+ *       ...
+ *     }
+ *   </ng-template>
+ * </formisch-field-array>
+ * ```
+ */
+@Component({
+  selector: 'formisch-field-array',
+  standalone: true,
+  imports: [NgIf, NgTemplateOutlet],
+  template: `
+    <ng-container
+      *ngIf="template"
+      [ngTemplateOutlet]="template"
+      [ngTemplateOutletContext]="{ $implicit: fieldArray }"
+    />
+  `,
+})
+export class FormischFieldArray<
+  TSchema extends Schema = Schema,
+  TFieldArrayPath extends RequiredPath = RequiredPath,
+> {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  @Input({ isSignal: true } as any) of: InputSignal<FormStore<TSchema>> = input.required<FormStore<TSchema>>();
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  @Input({ isSignal: true } as any) path: InputSignal<ValidArrayPath<v.InferInput<TSchema>, TFieldArrayPath>> = input.required<ValidArrayPath<v.InferInput<TSchema>, TFieldArrayPath>>();
+
+  @ContentChild(TemplateRef) protected template: TemplateRef<{ $implicit: FieldArrayStore<TSchema, TFieldArrayPath> }> | undefined;
+
+  private readonly internalFieldStore = computed(
+    () =>
+      getFieldStore(this.of()[INTERNAL], this.path()) as InternalArrayStore
+  );
+
+  protected readonly fieldArray: FieldArrayStore<TSchema, TFieldArrayPath> =
+    (() => {
+      const internalFieldStore = this.internalFieldStore;
+      const pathSignal = this.path;
+
+      return {
+        get path() {
+          return pathSignal();
+        },
+        items: computed(() => internalFieldStore().items.value),
+        errors: computed(() => internalFieldStore().errors.value),
+        isTouched: computed(() =>
+          getFieldBool(internalFieldStore(), 'isTouched')
+        ),
+        isDirty: computed(() =>
+          getFieldBool(internalFieldStore(), 'isDirty')
+        ),
+        isValid: computed(
+          () => !getFieldBool(internalFieldStore(), 'errors')
+        ),
+      };
+    })() as FieldArrayStore<TSchema, TFieldArrayPath>;
+}
