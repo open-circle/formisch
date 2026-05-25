8aef16b6824e8079935adfb91665ed919629edb4
Sonu Kapoor <sonukapoor@gmail.com>
2026-05-24 23:04:52 -0400

    Wire up src/index.ts with all public exports

--- frameworks/angular/src/index.ts
+++ frameworks/angular/src/index.ts
@@ -1,3 +1,18 @@
+export type {
+  DeepPartial,
+  FieldElement,
+  FormConfig,
+  PartialValues,
+  PathValue,
+  RequiredPath,
+  Schema,
+  SubmitEventHandler,
+  SubmitHandler,
+  ValidArrayPath,
+  ValidationMode,
+  ValidPath,
+} from '@formisch/core/angular';
+export * from '@formisch/methods/angular';
 export * from './components/index.ts';
 export * from './functions/index.ts';
 export * from './types/index.ts';
