f7cd575b92ec58803c7799a67758a4e7528a7973
Fabian Hiller <hillerfabian11@gmail.com>
2026-06-21 21:17:05 -0400

    Update the FormSchema type docs in our website

--- website/src/routes/(docs)/core/api/(types)/FormSchema/properties.ts
+++ website/src/routes/(docs)/core/api/(types)/FormSchema/properties.ts
@@ -7,73 +7,27 @@ export const properties: Record<string, PropertyProps> = {
       options: [
         {
           type: 'custom',
-          name: 'IntersectSchema',
-          href: 'https://valibot.dev/api/IntersectSchema/',
+          name: 'GenericSchema',
+          href: 'https://valibot.dev/api/GenericSchema/',
+          generics: [
+            {
+              type: 'custom',
+              name: 'Record',
+              generics: ['string', 'unknown'],
+            },
+          ],
         },
         {
           type: 'custom',
-          name: 'IntersectSchemaAsync',
-          href: 'https://valibot.dev/api/IntersectSchemaAsync/',
-        },
-        {
-          type: 'custom',
-          name: 'LazySchema',
-          href: 'https://valibot.dev/api/LazySchema/',
-        },
-        {
-          type: 'custom',
-          name: 'LazySchemaAsync',
-          href: 'https://valibot.dev/api/LazySchemaAsync/',
-        },
-        {
-          type: 'custom',
-          name: 'LooseObjectSchema',
-          href: 'https://valibot.dev/api/LooseObjectSchema/',
-        },
-        {
-          type: 'custom',
-          name: 'LooseObjectSchemaAsync',
-          href: 'https://valibot.dev/api/LooseObjectSchemaAsync/',
-        },
-        {
-          type: 'custom',
-          name: 'ObjectSchema',
-          href: 'https://valibot.dev/api/ObjectSchema/',
-        },
-        {
-          type: 'custom',
-          name: 'ObjectSchemaAsync',
-          href: 'https://valibot.dev/api/ObjectSchemaAsync/',
-        },
-        {
-          type: 'custom',
-          name: 'StrictObjectSchema',
-          href: 'https://valibot.dev/api/StrictObjectSchema/',
-        },
-        {
-          type: 'custom',
-          name: 'StrictObjectSchemaAsync',
-          href: 'https://valibot.dev/api/StrictObjectSchemaAsync/',
-        },
-        {
-          type: 'custom',
-          name: 'UnionSchema',
-          href: 'https://valibot.dev/api/UnionSchema/',
-        },
-        {
-          type: 'custom',
-          name: 'UnionSchemaAsync',
-          href: 'https://valibot.dev/api/UnionSchemaAsync/',
-        },
-        {
-          type: 'custom',
-          name: 'VariantSchema',
-          href: 'https://valibot.dev/api/VariantSchema/',
-        },
-        {
-          type: 'custom',
-          name: 'VariantSchemaAsync',
-          href: 'https://valibot.dev/api/VariantSchemaAsync/',
+          name: 'GenericSchemaAsync',
+          href: 'https://valibot.dev/api/GenericSchemaAsync/',
+          generics: [
+            {
+              type: 'custom',
+              name: 'Record',
+              generics: ['string', 'unknown'],
+            },
+          ],
         },
       ],
     },
